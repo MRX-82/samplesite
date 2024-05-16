3b2d48a43f377b95d3987325d8d34110e9ed334b
Who am I? <82019.is@gmail.com>
2024-05-16 21:07:51 +0300

    Написана форма form.py и внесены правки для вывода формы на веб-страницу, внесение в нее данных и сохранения в базу данных.

--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 from .models import Bb
 from .models import Rubric
+
+
+from django.views.generic.edit import CreateView
+
+from .forms import BbForm
+
 
 def by_rubric(request, rubric_id):
     bbs = Bb.objects.filter(rubric=rubric_id)
@@ -15,3 +22,14 @@ def index(request):
     rubrics = Rubric.objects.all()
     context = {'bbs': bbs, 'rubrics': rubrics}
     return render(request, 'bboard/index.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
